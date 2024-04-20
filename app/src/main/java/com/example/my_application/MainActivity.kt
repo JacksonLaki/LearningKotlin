1a1a5584263bf226edb0e96838c8c7620939f175
JacksonLaki <jacksonlazarocs@gmail.com>
2024-04-20 03:39:08 -0700

    MyFirstKotlinApp

---
+++ app/src/main/java/com/example/my_application/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.my_application
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val greetingTextView = findViewById<TextView>(R.id.tvHello)
+        val inputField = findViewById<EditText>(R.id.tvName)
+        val submitButton = findViewById<Button>(R.id.tvButton)
+        val offersButton = findViewById<Button>(R.id.btnOffers)
+        var name = ""
+        submitButton.setOnClickListener{
+           name = inputField.text.toString()
+            if (name == ""){
+                offersButton.visibility = INVISIBLE
+                greetingTextView.text = ""
+                Toast.makeText(this@MainActivity,
+                    "Please, enter your name!",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            }else {
+                val message = "Welcome $name"
+                greetingTextView.text = message
+                inputField.text.clear()
+                offersButton.visibility = VISIBLE
+            }
+        }
+
+        offersButton.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            intent.putExtra("USER",name)
+            startActivity(intent)
+
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/my_application/SecondActivity.kt
@@ -0,0 +1,29 @@
+package com.example.my_application
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        val userName = intent.getStringExtra("USER")
+        val textView = findViewById<TextView>(R.id.tvOffer)
+        val message = "$userName, You will get everything here!"
+        textView.text = message
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
