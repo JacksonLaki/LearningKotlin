3bc4a3a090d955d7aaecbbf18e8316cbfded63ad
JacksonLaki <jacksonlazarocs@gmail.com>
2024-04-21 23:32:09 -0700

    MyFirstKotlinApp

--- app/src/main/java/com/example/my_application/MainActivity.kt
+++ app/src/main/java/com/example/my_application/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.my_application
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.Button
@@ -12,6 +13,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.NonCancellable.message
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Log.i("MYTAG","MainActivity: OnCreate")
         val greetingTextView = findViewById<TextView>(R.id.tvHello)
         val inputField = findViewById<EditText>(R.id.tvName)
         val submitButton = findViewById<Button>(R.id.tvButton)
@@ -35,7 +38,9 @@ class MainActivity : AppCompatActivity() {
                     .show()
             }else {
                 val message = "Welcome $name"
+                Log.i("MYTAG",message)
                 greetingTextView.text = message
+                Log.i("MYTAG","After display the message on the text")
                 inputField.text.clear()
                 offersButton.visibility = VISIBLE
             }
