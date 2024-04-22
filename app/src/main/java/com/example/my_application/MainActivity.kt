1192b0e30ad8c9e34891328f9352f5d57864ce32
JacksonLaki <jacksonlazarocs@gmail.com>
2024-04-21 23:39:54 -0700

    MyFirstKotlinApp

--- app/src/main/java/com/example/my_application/MainActivity.kt
+++ app/src/main/java/com/example/my_application/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlinx.coroutines.NonCancellable.message
+
 
 
 class MainActivity : AppCompatActivity() {
